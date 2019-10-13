71d2474d991ded26e1323f417b694d51469fa7d1
NabeelTarek <nabeel.trq@gmail.com>
2019-10-13 11:10:02 +0400

    first commit

---
+++ RegistrationAndLogin/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationAndLogin.Models;
+
+namespace RegistrationAndLogin.Controllers
+{
+    public class HomeController : Controller
+    {
+        MyDatabaseEntities db = new MyDatabaseEntities();
+        // GET: Home
+        [Authorize]
+        public ActionResult Index()
+        {
+            if (Session["Token"] != null)
+            {
+                Guid g = Guid.NewGuid();
+                Session["Token"] = Convert.ToString(g);
+                string UserID = Session["UserID"].ToString();
+                var User = db.Users.Where(p => p.EmailID == UserID || p.IdCode == UserID).FirstOrDefault();
+                return View(User);
+            }
+            else
+            {
+                return RedirectToAction("Login", "User");
+            }            
+        }
+        public ActionResult TokenExpire()
+        {
+            Session.Remove("Token");            
+            return View();
+        }
+    }
+}
---
+++ RegistrationAndLogin/Models/UserLogin.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationAndLogin.Models
+{
+    public class UserLogin
+    {
+        [Display(Name ="Email ID")]
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Email/ID Code required")]
+        public string EmailID { get; set; }
+
+        [Display(Name = "ID Code")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email/ID Code required")]
+        public string IdCode { get; set; }
+
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Password required")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name ="Remember Me")]
+        public bool RememberMe { get; set; }
+    }
+}
